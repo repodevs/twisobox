79147e45eca141d676d7c3e3f6f62117685c183a
Edi Santoso <repodevs@gmail.com>
2021-08-04 12:18:53 +0700

    Add Tweets Outbox model

--- app/models/tweet.py
+++ app/models/tweet.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, ForeignKey, BigInteger, String, func
+from sqlalchemy import Column, ForeignKey, BigInteger, String, func, Text
+from sqlalchemy.dialects.postgresql import UUID, JSONB
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from app.db.base_class import Base
 
@@ -10,3 +11,13 @@ class Tweet(Base):
     sender_id = Column(BigInteger, ForeignKey("users.id"))
     text = Column(String)
     timestamp = Column(TIMESTAMP, nullable=False, server_default=func.now())
+
+
+class Outbox(Base):
+    __tablename__ = "tweets_outbox"
+
+    event_id = Column(UUID, primary_key=True, server_default=func.uuid_generate_v4())
+    aggregate_id = Column(Text)  # value is `aggregate_id` is from `tweets.id`
+    type = Column(Text)
+    aggregate = Column(JSONB)
+    created_at = Column(TIMESTAMP, server_default=func.now())
